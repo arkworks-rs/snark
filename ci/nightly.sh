e7975d02845d2682b7a6157714a71973113be4aa
debris <marek.kotewicz@gmail.com>
2020-01-31 11:59:17 +0100

    another attempt to fix travis-ci

--- ci/nightly.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-cargo fmt -- --check
-cargo test --all -- --skip dpc --skip integration_test
-cargo check --examples --all
--- ci/stable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-cargo test --all -- --skip dpc --skip integration_test
-cargo check --examples --all
