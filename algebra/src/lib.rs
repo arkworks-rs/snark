042698bf72bfa1b216d76fa45229372642f6675a
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-02-19 13:56:03 -0800

    Export pairing engines

--- algebra/src/lib.rs
+++ algebra/src/lib.rs
@@ -43,8 +43,13 @@ pub use algebra_core::*;
 
 #[cfg(feature = "bls12_377")]
 pub mod bls12_377;
+#[cfg(feature = "bls12_377")]
+pub use bls12_377::Bls12_377;
+
 #[cfg(feature = "bls12_381")]
 pub mod bls12_381;
+#[cfg(feature = "bls12_381")]
+pub use bls12_381::Bls12_381;
 
 #[cfg(feature = "bls12_377")]
 pub mod edwards_bls12;
@@ -54,11 +59,17 @@ pub mod jubjub;
 
 #[cfg(feature = "mnt6")]
 pub mod mnt6;
+#[cfg(feature = "mnt6")]
+pub use mnt6::MNT6;
+
 #[cfg(feature = "sw6")]
 pub mod sw6;
+#[cfg(feature = "sw6")]
+pub use  sw6::SW6;
 
 #[cfg(feature = "sw6")]
 pub mod edwards_sw6;
+
 
 #[cfg(test)]
 pub(crate) mod tests;
