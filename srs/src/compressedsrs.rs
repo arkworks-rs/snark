a829e1aa525f256015c2740f63bbaff3e123fe5c
Rebekah Mercer <github@rbkhmrcr.com>
2020-02-27 18:42:45 -0800

    type fiddling

--- srs/src/compressedsrs.rs
+++ srs/src/compressedsrs.rs
@@ -1,5 +1,11 @@
 mod groupmap;
 
+#[derive(Copy, Clone, Debug)]
+pub struct CompressedSRS<G: AffineCurve> {
+    pub y_coords: Vec<G::BaseField>,
+    pub x_bits: usize, // could be [bool; 2] ?
+}
+
 impl CompressedSRS {
     fn create(length: usize, num_y_coords: usize) -> Self {}
     fn decompress(c: Self) -> Vec<(G::BaseField, G::BaseField)> {}
--- srs/src/groupmap.rs
+++ srs/src/groupmap.rs
@@ -1,53 +1,55 @@
 use crate::CompressedSRS;
-use algebra::curves::{AffineCurve, PrimeField};
+use algebra::{
+    curves::{models::SWModelParameters, AffineCurve},
+    fields::Field,
+};
 use core::fmt::Error;
 
-#[derive(Copy, Clone, Debug)]
-pub struct CompressedSRS<G: AffineCurve> {
-    pub y_coords: Vec<G::BaseField>,
-    pub x_bits: usize, // could be [bool; 2] ?
+pub trait GroupMap<G: SWModelParameters> {
+    type Parameters;
+    fn setup() -> Self::Parameters;
+    fn batch_to_group_x(p: &Self::Parameters, ts: Vec<G::BaseField>) -> Vec<[G::BaseField; 3]>;
 }
 
-pub trait GroupMap<Params, G: AffineCurve> {
-    fn setup() -> Params;
-
-    fn batch_to_group_x(p: Params, ts: Vec<G::BaseField>) -> Vec<[G::BaseField; 3]>;
+struct BWParameters<G: SWModelParameters> {
+    u: G::BaseField,
+    fu: G::BaseField,
+    three_u_plus_u_over_2: G::BaseField,
+    three_u_minus_u_over_2: G::BaseField,
+    three_u: G::BaseField,
+    inv_three_u: G::BaseField,
 }
 
-impl GroupMap<Params, G: AffineCurve> {
+impl<G: SWModelParameters> GroupMap<G> for BWParameters<G> {
+    type Parameters = BWParameters<G>;
+    type FF = G::BaseField;
 
-    struct BWParameters<G> {
-        u: G::BaseField,
-        fu: G::BaseField,
-        three_u_plus_u_over_2: G::BaseField,
-        three_u_minus_u_over_2: G::BaseField,
-        three_u: G::BaseField,
-        inv_three_u: G::BaseField
-    }
-
-    fn get_u() -> G::BaseField {
-        for u in 0..G::P {
-            let x_zero = &u.square();
-            x_zero.mul(F::from_int(-3));
-            x_zero.sqrt();
-            x_zero.minus(&u);
-            x_zero.div(F::from_int(2));
-            let (x, y) = curve_eqn(x_zero);
-            if legendre(y) == 1 {
-                u
+    fn setup() -> Self::Parameters {
+        fn get_u<G: SWModelParameters>() -> Option<F> {
+            let mut u: u8;
+            for u in 0..200 {
+                let x_zero = (<Self::FF as Field>::from(u)).square();
+                x_zero.mul(G::BaseField::from(3));
+                x_zero.minus();
+                x_zero.sqrt();
+                x_zero.ub(&u);
+                x_zero.div(G::BaseField::from(2));
+                let (x, y) = curve_eqn(x_zero);
+                if y.legendre() == 1 {
+                    Some(G::BaseField::from(u))
+                };
             }
+            None
         }
-    }
 
-    // should this be generic? can we get rid of A_COEFF?
-    fn setup() -> Result<Self::BWParameters, Error> {
         let u = get_u();
         let fu = &u.mul(&u);
-        fu.add(&G::A_COEFF);
+        fu.add(&G::COEFF_A);
         fu.mul(&u);
-        fu.add(&G::B_COEFF);
+        fu.add(&G::COEFF_B);
         let three_u = &u.square();
-        three_u.mul(F::from_int(-3));
+        three_u.mul(G::BaseField::from(3));
+        three_u.minus();
         let inv_three_u = three_u.copy();
         three_u.sqrt(); // sqrt(-3u^2)
         inv_three_u.inv(); // 1/(-3u^2)
@@ -55,72 +57,71 @@ impl GroupMap<Params, G: AffineCurve> {
         let three_u_minus_u_over_2 = three_u.copy();
         three_u_plus_u_over_2.add(&u);
         three_u_minus_u_over_2.sub(&u);
-        three_u_plus_u_over_2.div(F::from_int(2)); // sqrt(-3u^2) + u / 2
-        three_u_minus_u_over_2.div(F::from_int(2)); // sqrt(-3u^2) - u / 2
+        three_u_plus_u_over_2.div(G::BaseField::from(2)); // sqrt(-3u^2) + u / 2
+        three_u_minus_u_over_2.div(G::BaseField::from(2)); // sqrt(-3u^2) - u / 2
 
-        Self::BWParameters {
+        Self::Parameters {
             u,
             fu,
             three_u_plus_u_over_2,
             three_u_minus_u_over_2,
             three_u,
-            inv_three_u
-        }
-
-   }
-
-    fn curve_eqn(x: G::BaseField) -> G {
-        G {
-            x,
-            y: ((x * x * x) + (G::A_COEFF * x) + G::B_COEFF).sqrt();
+            inv_three_u,
         }
     }
 
-    fn potential_xs(params : Self::BWParameters, t: G::BaseField) -> Result<Self::BWSurface, Error> {
-        let alpha_inv = &t.square();
-        alpha_inv.add(&params.fu);
-        alpha_inv.mul(&t.square());
-        // check alpha_inv non zero
-        let alpha = alpha_inv.copy();
-        alpha.inv(); // 1/alpha_inv = alpha
-
-        let temp = &t.square();
-        temp.square();
-        temp.mul(&alpha);
-        temp.(&params.three_u);
-        let x1 = params.three_u_minus_u_over_2.copy();
-        x1.sub(&temp);
-
-        let x2 = F::zero().sub(&params.u);
-        x2.sub(&x1);
-        
-        alpha_inv.square();
-        temp = &t.square();
-        temp.square();
-        temp.inv();
-        temp.mul(&params.inv_three_u);
-        temp.mul(&alpha_inv);
-        let x3 = params.u.copy();
-        x3.sub(&temp);
-
-        BWSurface {
-            x1,
-            x2,
-            x3
+    fn batch_to_group_x(p: &Self::Parameters, ts: Vec<G::BaseField>) -> Vec<[G::BaseField; 3]> {
+        fn curve_eqn(x: G::BaseField) -> G {
+            G {
+                x,
+                y: ((x * x * x) + (G::A_COEFF * x) + G::B_COEFF).sqrt(),
+            }
         }
 
-    }
+        fn potential_xs(
+            params: Self::BWParameters,
+            t: G::BaseField,
+        ) -> Result<Vec<[G::BaseField; 3]>, Error> {
+            let alpha_inv = &t.square();
+            alpha_inv.add(&params.fu);
+            alpha_inv.mul(&t.square());
+            // check alpha_inv non zero
+            let alpha = alpha_inv.copy();
+            alpha.inv(); // 1/alpha_inv = alpha
 
-    fn to_group(t: G::BaseField) -> Option<G> {
-        let (x1, x2, x3) = &Self::potential_xs(t);
-        if let Some(b1) = &Self::try_decode(x1) {
-            Some(b1)
-        } else if let Some(b2) = &Self::try_decode(x2) {
-            Some(b2)
-        } else if let Some(b3) = &Self::try_decode(x3) {
-            Some(b3)
-        } else {
-            None
+            let temp = &t.square();
+            temp.square();
+            temp.mul(&alpha);
+            temp.add(&params.three_u);
+            let x1 = params.three_u_minus_u_over_2.copy();
+            x1.sub(&temp);
+
+            let x2 = F::zero().sub(&params.u);
+            x2.sub(&x1);
+
+            alpha_inv.square();
+            temp = &t.square();
+            temp.square();
+            temp.inv();
+            temp.mul(&params.inv_three_u);
+            temp.mul(&alpha_inv);
+            let x3 = params.u.copy();
+            x3.sub(&temp);
+
+            BWSurface { x1, x2, x3 }
+        }
+
+        fn to_group(t: G::BaseField) -> Option<G> {
+            let (x1, x2, x3) = &Self::potential_xs(t);
+            if let Some(b1) = &Self::try_decode(x1) {
+                Some(b1)
+            } else if let Some(b2) = &Self::try_decode(x2) {
+                Some(b2)
+            } else if let Some(b3) = &Self::try_decode(x3) {
+                Some(b3)
+            } else {
+                None
+            }
         }
     }
 }
