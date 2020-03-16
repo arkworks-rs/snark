9de9734b14bfb6feba885a4a3c05cb4260e0283f
Izaak Meckler <ihmeckler@gmail.com>
2020-03-16 16:22:39 -0700

    add batch test

--- srs/tests/groupmap.rs
+++ srs/tests/groupmap.rs
@@ -19,3 +19,25 @@ fn test_group_map_on_curve() {
     let g = G1Affine::new(x, y, false);
     assert!(g.is_on_curve());
 }
+
+fn first_xy(xs: & [Fq; 3]) -> (Fq, Fq) {
+    for x in xs.iter() {
+        match srs::groupmap::get_y::<G>(*x) {
+            Some(y) => return (*x, y),
+            None => ()
+        }
+    }
+    panic!("get_xy")
+}
+
+#[test]
+fn test_batch_group_map_on_curve() {
+    let n = 1000;
+    let params =  BWParameters::<G>::setup();
+    let ts : Vec<Fq> = (0..1000).map(|_| rand::random()).collect();
+    for xs in BWParameters::<G>::batch_to_group_x(&params, ts).iter() {
+        let (x, y) = first_xy(xs);
+        let g = G1Affine::new(x, y, false);
+        assert!(g.is_on_curve());
+    };
+}
