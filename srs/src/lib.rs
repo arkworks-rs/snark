82a591871aab4c970b907979a9f895a0f26e16f0
Izaak Meckler <ihmeckler@gmail.com>
2020-03-16 17:26:31 -0700

    rename

--- srs/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod groupmap;
-
-use algebra::curves::AffineCurve;
-
-pub trait CompressedSRS<G: AffineCurve> {
-    fn create(length: usize, num_y_coords: usize) -> Self;
-    fn decompress(c: Self) -> Vec<(G::BaseField, G::BaseField)>;
-}
