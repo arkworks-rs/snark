535b4425fe3944514700c26458b48ac485053741
Weikeng Chen <w.k@berkeley.edu>
2020-03-26 14:04:42 -0700

    Update uint64.rs

--- r1cs-std/src/bits/uint64.rs
+++ r1cs-std/src/bits/uint64.rs
@@ -338,13 +338,6 @@ impl<ConstraintF: Field> ToBytesGadget<ConstraintF> for UInt64 {
 
         Ok(bytes)
     }
-
-    fn to_bytes_strict<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        cs: CS,
-    ) -> Result<Vec<UInt8>, SynthesisError> {
-        self.to_bytes(cs)
-    }
 }
 
 impl PartialEq for UInt64 {
