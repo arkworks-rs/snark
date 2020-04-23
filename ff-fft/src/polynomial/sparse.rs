f1e65cdbe4073ee08de7af28d2058b2cb3818499
jon-chuang <9093549+jon-chuang@users.noreply.github.com>
2020-04-23 09:03:28 +0800

    minor changes

--- ff-fft/src/polynomial/sparse.rs
+++ ff-fft/src/polynomial/sparse.rs
@@ -18,16 +18,16 @@ pub struct SparsePolynomial<F: Field> {
 
 impl<F: Field> fmt::Debug for SparsePolynomial<F> {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        for (i, coeff) in self.values.iter().rev().filter(|(_, x)| **x != 0) {
+        for (i, coeff) in self.coeffs.iter().rev().filter(|(_, x)| **x != 0) {
             if i != self.values.len()-1 {
                 write!(f, " + ");
             }
             if i == 0 {
                 write!(f, "{:?}", coeff)?;
             } else if i == 1 {
-                write!(f, "{} * x", if !coeff.is_one() { coeff.to_string() } else { "".to_string() })?;
+                write!(f, "{}*x", if !coeff.is_one() { coeff.to_string() } else { "".to_string() })?;
             } else {
-                write!(f, "{} * x^{}", if !coeff.is_one() { coeff.to_string() } else { "".to_string() }, i)?;
+                write!(f, "{}*x^{}", if !coeff.is_one() { coeff.to_string() } else { "".to_string() }, i)?;
             }
         }
         Ok(())
