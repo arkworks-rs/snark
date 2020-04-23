7f800325d46e77b196690eb868d361439bb53dd6
jon-chuang <9093549+jon-chuang@users.noreply.github.com>
2020-04-23 15:29:08 +0800

    more readable string manipulation

--- algebra-core/mince/src/intrinsics.rs
+++ algebra-core/mince/src/intrinsics.rs
@@ -5,41 +5,41 @@ pub fn define_intrinsics() -> TokenStream {
     (quote! {
         {
             let mut begin = || {
-                asm_string.replace_with(|_| "\"".to_string());
+                asm_string.borrow_mut().push_str("\"");
             };
 
             let mut end = || {
-                asm_string.replace_with(|x| format!("{}{}", x, "
-                                        \"".to_string()));
+                asm_string.borrow_mut().push_str("
+                                        \"");
             };
 
             let mut comment = | comment: &str | {
-                asm_string.replace_with(|x| format!("{}{}", x, format!("         // {}", comment)));
+                asm_string.borrow_mut().push_str(&format!("         // {}", comment));
             };
 
             let mut mulxq = | a: &str, b: &str, c: &str | {
-                asm_string.replace_with(|x| format!("{}{}", x, format!("
-                                        mulxq {}, {}, {}", a, b, c)));
+                asm_string.borrow_mut().push_str(&format!("
+                                        mulxq {}, {}, {}", a, b, c));
             };
 
             let mut adcxq = | a: &str, b: &str| {
-                asm_string.replace_with(|x| format!("{}{}", x, format!("
-                                        adcxq {}, {}", a, b)));
+                asm_string.borrow_mut().push_str(&format!("
+                                        adcxq {}, {}", a, b));
             };
 
             let mut adoxq = | a: &str, b: &str | {
-                asm_string.replace_with(|x| format!("{}{}", x, format!("
-                                        adoxq {}, {}", a, b)));
+                asm_string.borrow_mut().push_str(&format!("
+                                        adoxq {}, {}", a, b));
             };
 
             let mut movq = | a: &str, b: &str | {
-                asm_string.replace_with(|x| format!("{}{}", x, format!("
-                                        movq {}, {}", a, b)));
+                asm_string.borrow_mut().push_str(&format!("
+                                        movq {}, {}", a, b));
             };
 
             let mut xorq = | a: &str, b: &str | {
-                asm_string.replace_with(|x| format!("{}{}", x, format!("
-                                        xorq {}, {}", a, b)));
+                asm_string.borrow_mut().push_str(&format!("
+                                        xorq {}, {}", a, b));
             };
         }
     }).into()
