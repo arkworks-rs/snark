20495817ba91f19b4fd33526d6a2a6bf599ef9e4
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-05-05 10:41:59 -0700

    Update build.rs

--- algebra-benches/build.rs
+++ algebra-benches/build.rs
@@ -3,7 +3,7 @@ extern crate rustc_version;
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    if version_meta().channel == Channel::Nightly {
+    if version_meta().channel.expect("nightly check failed") == Channel::Nightly {
         println!("cargo:rustc-cfg=nightly");
     }
 }
