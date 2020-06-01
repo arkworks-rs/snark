11ba430ba2870f7d7bda02dacbc7083488655cee
Youssef El Housni <youssef.housni21@gmail.com>
2020-06-01 19:25:40 +0200

    perf: 2-NAF only for the second ML

--- algebra/src/bw6_761/curves/mod.rs
+++ algebra/src/bw6_761/curves/mod.rs
@@ -30,11 +30,7 @@ impl BW6Parameters for Parameters {
     /// `x` is positive.
     const X_IS_NEGATIVE: bool = false;
     // X+1
-    const ATE_LOOP_COUNT_1: &'static [i8] = &[
-        0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0,
-        0, 0, 0, 1,
-    ];
+    const ATE_LOOP_COUNT_1: &'static [u64] = &[0x8508c00000000002];
     const ATE_LOOP_COUNT_1_IS_NEGATIVE: bool = false;
     // X^3-X^2-X
     const ATE_LOOP_COUNT_2: &'static [i8] = &[
