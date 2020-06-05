159888945f8d367311119a819f970399d2bdebfc
DanieleDiBenedetto <daniele@horizenlabs.io>
2020-06-05 12:56:52 -0400

    Added and modified tests

--- proof-systems/src/groth16/test.rs
+++ proof-systems/src/groth16/test.rs
@@ -34,32 +34,32 @@ impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for MySillyCircuit<C
     }
 }
 
-mod bls12_377 {
+mod test{
     use super::*;
     use crate::groth16::{
         Parameters, Proof, VerifyingKey, PreparedVerifyingKey,
         create_random_proof, generate_random_parameters, prepare_verifying_key, verify_proof,
     };
 
-    use algebra::{curves::bls12_377::Bls12_377, fields::bls12_377::Fr, UniformRand,
-            ToBytes, FromBytes, to_bytes,
+    use algebra::{
+        curves::{bls12_377::Bls12_377, sw6::SW6, mnt4753::MNT4, mnt6753::MNT6},
+        UniformRand, ToBytes, FromBytes, to_bytes, PairingEngine
     };
     use rand::thread_rng;
     use std::ops::MulAssign;
 
-    #[test]
-    fn prove_and_verify() {
+    fn test_prove_and_verify<E: PairingEngine>() {
         let rng = &mut thread_rng();
 
         let params =
-            generate_random_parameters::<Bls12_377, _, _>(MySillyCircuit { a: None, b: None }, rng)
+            generate_random_parameters::<E, _, _>(MySillyCircuit { a: None, b: None }, rng)
                 .unwrap();
 
-        let pvk = prepare_verifying_key::<Bls12_377>(&params.vk);
+        let pvk = prepare_verifying_key::<E>(&params.vk);
 
         for _ in 0..100 {
-            let a = Fr::rand(rng);
-            let b = Fr::rand(rng);
+            let a = <E as PairingEngine>::Fr::rand(rng);
+            let b = <E as PairingEngine>::Fr::rand(rng);
             let mut c = a;
             c.mul_assign(&b);
 
@@ -71,35 +71,34 @@ mod bls12_377 {
                 &params,
                 rng,
             )
-            .unwrap();
+                .unwrap();
 
             assert!(verify_proof(&pvk, &proof, &[c]).unwrap());
             assert!(!verify_proof(&pvk, &proof, &[a]).unwrap());
         }
     }
 
-    #[test]
-    fn serialize_deserialize() {
+    fn test_serialize_deserialize<E: PairingEngine>() -> (Vec<u8>, Vec<u8>, Vec<u8>) {
 
         let rng = &mut thread_rng();
 
         let params =
-            generate_random_parameters::<Bls12_377, _, _>(MySillyCircuit { a: None, b: None }, rng)
+            generate_random_parameters::<E, _, _>(MySillyCircuit { a: None, b: None }, rng)
                 .unwrap();
 
         let vk = params.vk.clone();
 
         let params_serialized = to_bytes!(params).unwrap();
-        let params_deserialized = Parameters::<Bls12_377>::read(params_serialized.as_slice()).unwrap();
+        let params_deserialized = Parameters::<E>::read(params_serialized.as_slice()).unwrap();
         assert_eq!(params, params_deserialized);
 
         let vk_serialized = to_bytes!(vk).unwrap();
-        let vk_deserialized = VerifyingKey::<Bls12_377>::read(vk_serialized.as_slice()).unwrap();
+        let vk_deserialized = VerifyingKey::<E>::read(vk_serialized.as_slice()).unwrap();
         assert_eq!(vk, vk_deserialized);
 
 
-        let a = Fr::rand(rng);
-        let b = Fr::rand(rng);
+        let a = <E as PairingEngine>::Fr::rand(rng);
+        let b = <E as PairingEngine>::Fr::rand(rng);
         let c = a * &b;
 
         let proof = create_random_proof(
@@ -113,276 +112,88 @@ mod bls12_377 {
             .unwrap();
 
         let proof_serialized = to_bytes!(proof).unwrap();
-        let proof_deserialized = Proof::<Bls12_377>::read(proof_serialized.as_slice()).unwrap();
+        let proof_deserialized = Proof::<E>::read(proof_serialized.as_slice()).unwrap();
         assert_eq!(proof, proof_deserialized);
 
         let pvk = prepare_verifying_key(&vk_deserialized);
         let pvk_serialized = to_bytes!(pvk).unwrap();
-        let pvk_deserialized = PreparedVerifyingKey::<Bls12_377>::read(pvk_serialized.as_slice()).unwrap();
+        let pvk_deserialized = PreparedVerifyingKey::<E>::read(pvk_serialized.as_slice()).unwrap();
         assert_eq!(pvk, pvk_deserialized);
 
         assert!(verify_proof(&pvk_deserialized, &proof_deserialized, &[c]).unwrap());
+
+        (params_serialized, proof_serialized, vk_serialized)
     }
-}
-
-mod sw6 {
-    use super::*;
-    use crate::groth16::{
-        Parameters, Proof, VerifyingKey, PreparedVerifyingKey,
-        create_random_proof, generate_random_parameters, prepare_verifying_key, verify_proof,
-    };
-
-    use rand::thread_rng;
-
-    use algebra::{curves::sw6::SW6, fields::sw6::Fr as SW6Fr, Field, UniformRand,
-                  ToBytes, FromBytes, to_bytes,
-    };
 
     #[test]
-    fn prove_and_verify() {
-        let rng = &mut thread_rng();
-
-        let params =
-            generate_random_parameters::<SW6, _, _>(MySillyCircuit { a: None, b: None }, rng)
-                .unwrap();
-
-        let pvk = prepare_verifying_key::<SW6>(&params.vk);
-
-        let a = SW6Fr::rand(rng);
-        let b = SW6Fr::rand(rng);
-        let c = a * &b;
-
-        let proof = create_random_proof(
-            MySillyCircuit {
-                a: Some(a),
-                b: Some(b),
-            },
-            &params,
-            rng,
-        )
-        .unwrap();
-
-        assert!(verify_proof(&pvk, &proof, &[c]).unwrap());
-        assert!(!verify_proof(&pvk, &proof, &[SW6Fr::zero()]).unwrap());
+    fn prove_verify() {
+        test_prove_and_verify::<Bls12_377>();
+        test_prove_and_verify::<SW6>();
+        test_prove_and_verify::<MNT4>();
+        test_prove_and_verify::<MNT6>();
     }
 
     #[test]
     fn serialize_deserialize() {
 
-        let rng = &mut thread_rng();
+        // Basic checks
+        let (params_bls, proof_bls, vk_bls) = test_serialize_deserialize::<Bls12_377>();
+        let (params_sw6, proof_sw6, vk_sw6) = test_serialize_deserialize::<SW6>();
+        let (params_mnt4, proof_mnt4, vk_mnt4) = test_serialize_deserialize::<MNT4>();
+        let (params_mnt6, proof_mnt6, vk_mnt6) = test_serialize_deserialize::<MNT6>();
 
-        let params =
-            generate_random_parameters::<SW6, _, _>(MySillyCircuit { a: None, b: None }, rng)
-                .unwrap();
+        // Cross checks: let's assert that deserialization fails when composing fields/points are not valid
 
-        let vk = params.vk.clone();
+        // BLS12-377
+        assert!(Parameters::<Bls12_377>::read(params_sw6.as_slice()).is_err());
+        assert!(Parameters::<Bls12_377>::read(params_mnt4.as_slice()).is_err());
+        assert!(Parameters::<Bls12_377>::read(params_mnt6.as_slice()).is_err());
 
-        let params_serialized = to_bytes!(params).unwrap();
-        let params_deserialized = Parameters::<SW6>::read(params_serialized.as_slice()).unwrap();
-        assert_eq!(params, params_deserialized);
+        assert!(Proof::<Bls12_377>::read(proof_sw6.as_slice()).is_err());
+        assert!(Proof::<Bls12_377>::read(proof_mnt4.as_slice()).is_err());
+        assert!(Proof::<Bls12_377>::read(proof_mnt6.as_slice()).is_err());
 
-        let vk_serialized = to_bytes!(vk).unwrap();
-        let vk_deserialized = VerifyingKey::<SW6>::read(vk_serialized.as_slice()).unwrap();
-        assert_eq!(vk, vk_deserialized);
+        assert!(VerifyingKey::<Bls12_377>::read(vk_sw6.as_slice()).is_err());
+        assert!(VerifyingKey::<Bls12_377>::read(vk_mnt4.as_slice()).is_err());
+        assert!(VerifyingKey::<Bls12_377>::read(vk_mnt6.as_slice()).is_err());
 
+        // SW6
+        assert!(Parameters::<SW6>::read(params_bls.as_slice()).is_err());
+        assert!(Parameters::<SW6>::read(params_mnt4.as_slice()).is_err());
+        assert!(Parameters::<SW6>::read(params_mnt6.as_slice()).is_err());
 
-        let a = SW6Fr::rand(rng);
-        let b = SW6Fr::rand(rng);
-        let c = a * &b;
+        assert!(Proof::<SW6>::read(proof_bls.as_slice()).is_err());
+        assert!(Proof::<SW6>::read(proof_mnt4.as_slice()).is_err());
+        assert!(Proof::<SW6>::read(proof_mnt6.as_slice()).is_err());
 
-        let proof = create_random_proof(
-            MySillyCircuit {
-                a: Some(a),
-                b: Some(b),
-            },
-            &params_deserialized,
-            rng,
-        )
-            .unwrap();
+        assert!(VerifyingKey::<SW6>::read(vk_bls.as_slice()).is_err());
+        assert!(VerifyingKey::<SW6>::read(vk_mnt4.as_slice()).is_err());
+        assert!(VerifyingKey::<SW6>::read(vk_mnt6.as_slice()).is_err());
 
-        let proof_serialized = to_bytes!(proof).unwrap();
-        let proof_deserialized = Proof::<SW6>::read(proof_serialized.as_slice()).unwrap();
-        assert_eq!(proof, proof_deserialized);
+        //MNT4-753
+        assert!(Parameters::<MNT4>::read(params_bls.as_slice()).is_err());
+        assert!(Parameters::<MNT4>::read(params_sw6.as_slice()).is_err());
+        assert!(Parameters::<MNT4>::read(params_mnt6.as_slice()).is_err());
 
-        let pvk = prepare_verifying_key(&vk_deserialized);
-        let pvk_serialized = to_bytes!(pvk).unwrap();
-        let pvk_deserialized = PreparedVerifyingKey::<SW6>::read(pvk_serialized.as_slice()).unwrap();
-        assert_eq!(pvk, pvk_deserialized);
+        assert!(Proof::<MNT4>::read(proof_bls.as_slice()).is_err());
+        assert!(Proof::<MNT4>::read(proof_sw6.as_slice()).is_err());
+        assert!(Proof::<MNT4>::read(proof_mnt6.as_slice()).is_err());
 
-        assert!(verify_proof(&pvk_deserialized, &proof_deserialized, &[c]).unwrap());
-    }
-}
+        assert!(VerifyingKey::<MNT4>::read(vk_bls.as_slice()).is_err());
+        assert!(VerifyingKey::<MNT4>::read(vk_sw6.as_slice()).is_err());
+        assert!(VerifyingKey::<MNT4>::read(vk_mnt6.as_slice()).is_err());
 
-mod mnt4753 {
-    use super::*;
-    use crate::groth16::{
-        Parameters, Proof, VerifyingKey, PreparedVerifyingKey,
-        create_random_proof, generate_random_parameters, prepare_verifying_key, verify_proof,
-    };
+        //MNT6-753
+        assert!(Parameters::<MNT6>::read(params_bls.as_slice()).is_err());
+        assert!(Parameters::<MNT6>::read(params_sw6.as_slice()).is_err());
+        assert!(Parameters::<MNT6>::read(params_mnt4.as_slice()).is_err());
 
-    use rand::thread_rng;
+        assert!(Proof::<MNT6>::read(proof_bls.as_slice()).is_err());
+        assert!(Proof::<MNT6>::read(proof_sw6.as_slice()).is_err());
+        assert!(Proof::<MNT6>::read(proof_mnt4.as_slice()).is_err());
 
-    use algebra::{curves::mnt4753::MNT4, fields::mnt4753::Fr as MNT4Fr, Field, UniformRand,
-                  ToBytes, FromBytes, to_bytes};
-
-    #[test]
-    fn prove_and_verify() {
-        let rng = &mut thread_rng();
-
-        let params =
-            generate_random_parameters::<MNT4, _, _>(MySillyCircuit { a: None, b: None }, rng)
-                .unwrap();
-
-        let pvk = prepare_verifying_key::<MNT4>(&params.vk);
-
-        let a = MNT4Fr::rand(rng);
-        let b = MNT4Fr::rand(rng);
-        let c = a * &b;
-
-        let proof = create_random_proof(
-            MySillyCircuit {
-                a: Some(a),
-                b: Some(b),
-            },
-            &params,
-            rng,
-        )
-            .unwrap();
-
-        assert!(verify_proof(&pvk, &proof, &[c]).unwrap());
-        assert!(!verify_proof(&pvk, &proof, &[MNT4Fr::zero()]).unwrap());
-    }
-
-    #[test]
-    fn serialize_deserialize() {
-
-        let rng = &mut thread_rng();
-
-        let params =
-            generate_random_parameters::<MNT4, _, _>(MySillyCircuit { a: None, b: None }, rng)
-                .unwrap();
-
-        let vk = params.vk.clone();
-
-        let params_serialized = to_bytes!(params).unwrap();
-        let params_deserialized = Parameters::<MNT4>::read(params_serialized.as_slice()).unwrap();
-        assert_eq!(params, params_deserialized);
-
-        let vk_serialized = to_bytes!(vk).unwrap();
-        let vk_deserialized = VerifyingKey::<MNT4>::read(vk_serialized.as_slice()).unwrap();
-        assert_eq!(vk, vk_deserialized);
-
-
-        let a = MNT4Fr::rand(rng);
-        let b = MNT4Fr::rand(rng);
-        let c = a * &b;
-
-        let proof = create_random_proof(
-            MySillyCircuit {
-                a: Some(a),
-                b: Some(b),
-            },
-            &params_deserialized,
-            rng,
-        )
-            .unwrap();
-
-        let proof_serialized = to_bytes!(proof).unwrap();
-        let proof_deserialized = Proof::<MNT4>::read(proof_serialized.as_slice()).unwrap();
-        assert_eq!(proof, proof_deserialized);
-
-        let pvk = prepare_verifying_key(&vk_deserialized);
-        let pvk_serialized = to_bytes!(pvk).unwrap();
-        let pvk_deserialized = PreparedVerifyingKey::<MNT4>::read(pvk_serialized.as_slice()).unwrap();
-        assert_eq!(pvk, pvk_deserialized);
-
-        assert!(verify_proof(&pvk_deserialized, &proof_deserialized, &[c]).unwrap());
-    }
-}
-
-mod mnt6753 {
-    use super::*;
-    use crate::groth16::{
-        Parameters, Proof, VerifyingKey, PreparedVerifyingKey,
-        create_random_proof, generate_random_parameters, prepare_verifying_key, verify_proof,
-    };
-
-    use rand::thread_rng;
-
-    use algebra::{curves::mnt6753::MNT6, fields::mnt6753::Fr as MNT6Fr, Field, UniformRand,
-                  ToBytes, FromBytes, to_bytes,};
-
-    #[test]
-    fn prove_and_verify() {
-        let rng = &mut thread_rng();
-
-        let params =
-            generate_random_parameters::<MNT6, _, _>(MySillyCircuit { a: None, b: None }, rng)
-                .unwrap();
-
-        let pvk = prepare_verifying_key::<MNT6>(&params.vk);
-
-        let a = MNT6Fr::rand(rng);
-        let b = MNT6Fr::rand(rng);
-        let c = a * &b;
-
-        let proof = create_random_proof(
-            MySillyCircuit {
-                a: Some(a),
-                b: Some(b),
-            },
-            &params,
-            rng,
-        )
-            .unwrap();
-
-        assert!(verify_proof(&pvk, &proof, &[c]).unwrap());
-        assert!(!verify_proof(&pvk, &proof, &[MNT6Fr::zero()]).unwrap());
-    }
-
-    #[test]
-    fn serialize_deserialize() {
-
-        let rng = &mut thread_rng();
-
-        let params =
-            generate_random_parameters::<MNT6, _, _>(MySillyCircuit { a: None, b: None }, rng)
-                .unwrap();
-
-        let vk = params.vk.clone();
-
-        let params_serialized = to_bytes!(params).unwrap();
-        let params_deserialized = Parameters::<MNT6>::read(params_serialized.as_slice()).unwrap();
-        assert_eq!(params, params_deserialized);
-
-        let vk_serialized = to_bytes!(vk).unwrap();
-        let vk_deserialized = VerifyingKey::<MNT6>::read(vk_serialized.as_slice()).unwrap();
-        assert_eq!(vk, vk_deserialized);
-
-
-        let a = MNT6Fr::rand(rng);
-        let b = MNT6Fr::rand(rng);
-        let c = a * &b;
-
-        let proof = create_random_proof(
-            MySillyCircuit {
-                a: Some(a),
-                b: Some(b),
-            },
-            &params_deserialized,
-            rng,
-        )
-            .unwrap();
-
-        let proof_serialized = to_bytes!(proof).unwrap();
-        let proof_deserialized = Proof::<MNT6>::read(proof_serialized.as_slice()).unwrap();
-        assert_eq!(proof, proof_deserialized);
-
-        let pvk = prepare_verifying_key(&vk_deserialized);
-        let pvk_serialized = to_bytes!(pvk).unwrap();
-        let pvk_deserialized = PreparedVerifyingKey::<MNT6>::read(pvk_serialized.as_slice()).unwrap();
-        assert_eq!(pvk, pvk_deserialized);
-
-        assert!(verify_proof(&pvk_deserialized, &proof_deserialized, &[c]).unwrap());
+        assert!(VerifyingKey::<MNT6>::read(vk_bls.as_slice()).is_err());
+        assert!(VerifyingKey::<MNT6>::read(vk_sw6.as_slice()).is_err());
+        assert!(VerifyingKey::<MNT6>::read(vk_mnt4.as_slice()).is_err());
     }
 }
