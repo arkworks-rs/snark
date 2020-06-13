484daf2f580d2fe84896425ac7e128e1558931c8
weikeng <w.k@berkeley.edu>
2020-06-12 19:58:54 -0700

    labels

--- algebra/src/mnt6_753/fields/mod.rs
+++ algebra/src/mnt6_753/fields/mod.rs
@@ -1,11 +1,11 @@
-#[cfg(any(feature = "mnt6_753", feature = "mnt4_753"))]
+#[cfg(any(feature = "mnt6_753", feature = "mnt4_753", feature = "ed_on_mnt4_753"))]
 pub mod fr;
-#[cfg(any(feature = "mnt6_753", feature = "mnt4_753"))]
+#[cfg(any(feature = "mnt6_753", feature = "mnt4_753", feature = "ed_on_mnt4_753"))]
 pub use self::fr::*;
 
-#[cfg(any(feature = "mnt6_753", feature = "mnt4_753"))]
+#[cfg(any(feature = "mnt6_753", feature = "mnt4_753", feature = "ed_on_mnt4_753"))]
 pub mod fq;
-#[cfg(any(feature = "mnt6_753", feature = "mnt4_753"))]
+#[cfg(any(feature = "mnt6_753", feature = "mnt4_753", feature = "ed_on_mnt4_753"))]
 pub use self::fq::*;
 
 #[cfg(feature = "mnt6_753")]
--- algebra/src/mnt6_753/mod.rs
+++ algebra/src/mnt6_753/mod.rs
@@ -19,10 +19,10 @@
 
 #[cfg(feature = "mnt6_753")]
 mod curves;
-#[cfg(any(feature = "mnt6_753", feature = "mnt4_753"))]
+#[cfg(any(feature = "mnt6_753", feature = "mnt4_753", feature = "ed_on_mnt4_753"))]
 mod fields;
 
 #[cfg(feature = "mnt6_753")]
 pub use curves::*;
-#[cfg(any(feature = "mnt6_753", feature = "mnt4_753"))]
+#[cfg(any(feature = "mnt6_753", feature = "mnt4_753", feature = "ed_on_mnt4_753"))]
 pub use fields::*;
