ecc6057971c1bb1595ca5c5537afc5e0561a38d5
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-08-24 00:43:20 -0700

    Rework `r1cs-core`.

--- r1cs-core/src/impl_lc.rs
+++ r1cs-core/src/impl_lc.rs
@@ -1,55 +1,71 @@
-use crate::{Index, LinearCombination, SmallVec, Variable};
+use crate::{LinearCombination, Variable, Vec};
 use algebra_core::Field;
-use core::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub};
-use smallvec::smallvec;
+use core::ops::{Add, AddAssign, Deref, DerefMut, Mul, MulAssign, Neg, Sub};
 
-impl<F: Field> AsRef<[(Variable, F)]> for LinearCombination<F> {
+impl<F: Field> LinearCombination<F> {
+    /// Create a new empty linear combination.
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    /// Create a new empty linear combination.
+    pub fn zero() -> Self {
+        Self(Vec::new())
+    }
+
+    /// Deduplicate entries in `self`.
+    pub fn compactify(&mut self) {
+        self.0.sort_by_key(|e| e.1);
+        let mut current_var = None;
+        let mut current_var_first_index = 0;
+        for i in 0..self.0.len() {
+            let (f, v) = self.0[i];
+            if Some(v) == current_var {
+                self.0[current_var_first_index].0 += &f;
+            } else {
+                current_var = Some(v);
+                current_var_first_index = i;
+            }
+        }
+        self.0.dedup_by_key(|e| e.1);
+    }
+}
+
+impl<'a, F: Field> Deref for LinearCombination<F> {
+    type Target = Vec<(F, Variable)>;
+
     #[inline]
-    fn as_ref(&self) -> &[(Variable, F)] {
+    fn deref(&self) -> &Vec<(F, Variable)> {
         &self.0
+    }
+}
+
+impl<F: Field> DerefMut for LinearCombination<F> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
 
 impl<F: Field> From<(F, Variable)> for LinearCombination<F> {
     #[inline]
-    fn from((coeff, var): (F, Variable)) -> Self {
-        LinearCombination(smallvec![(var, coeff)])
+    fn from(input: (F, Variable)) -> Self {
+        LinearCombination(crate::vec![input])
     }
 }
 
 impl<F: Field> From<Variable> for LinearCombination<F> {
     #[inline]
     fn from(var: Variable) -> Self {
-        LinearCombination(smallvec![(var, F::one())])
+        LinearCombination(crate::vec![(F::one(), var)])
     }
 }
 
 impl<F: Field> LinearCombination<F> {
-    /// Outputs an empty linear combination.
-    #[inline]
-    pub fn zero() -> LinearCombination<F> {
-        LinearCombination(SmallVec::<F>::new())
-    }
-
-    /// Replaces the contents of `self` with those of `other`.
-    #[inline]
-    pub fn replace_in_place(&mut self, other: Self) {
-        self.0.truncate(0);
-        self.0.extend_from_slice(&other.0)
-    }
-
     /// Negate the coefficients of all variables in `self`.
     #[inline]
     pub fn negate_in_place(&mut self) {
-        self.0.iter_mut().for_each(|(_, coeff)| *coeff = -(*coeff));
-    }
-
-    /// Double the coefficients of all variables in `self`.
-    #[inline]
-    pub fn double_in_place(&mut self) {
-        self.0.iter_mut().for_each(|(_, coeff)| {
-            coeff.double_in_place();
-        });
+        self.0.iter_mut().for_each(|(coeff, _)| *coeff = -(*coeff));
     }
 
     /// Get the location of a variable in `self`.
@@ -57,7 +73,7 @@ impl<F: Field> LinearCombination<F> {
     pub fn get_var_loc(&self, search_var: &Variable) -> Result<usize, usize> {
         if self.0.len() < 6 {
             let mut found_index = 0;
-            for (i, (var, _)) in self.0.iter().enumerate() {
+            for (i, (_, var)) in self.iter().enumerate() {
                 if var >= search_var {
                     found_index = i;
                     break;
@@ -68,28 +84,8 @@ impl<F: Field> LinearCombination<F> {
             Err(found_index)
         } else {
             self.0
-                .binary_search_by_key(search_var, |&(cur_var, _)| cur_var)
+                .binary_search_by_key(search_var, |&(_, cur_var)| cur_var)
         }
-    }
-
-    /// Return whether or not a particular linear combination represents a constant
-    #[inline]
-    pub fn is_constant(&self) -> bool {
-        // Linear combination is 0
-        if self.0.len() == 0 {
-            return true;
-        }
-        // Linear combination has more than one variable, so it must not be a constant
-        if self.0.len() != 1 {
-            return false;
-        }
-
-        // Check if the only variable in the LC is the variable corresponding to constants
-        let var = self.0[0].0;
-        if var.0 != Index::Input(0) {
-            return false;
-        }
-        true
     }
 }
 
@@ -107,8 +103,8 @@ impl<F: Field> AddAssign<(F, Variable)> for LinearCombination<F> {
     #[inline]
     fn add_assign(&mut self, (coeff, var): (F, Variable)) {
         match self.get_var_loc(&var) {
-            Ok(found) => self.0[found].1 += &coeff,
-            Err(not_found) => self.0.insert(not_found, (var, coeff)),
+            Ok(found) => self.0[found].0 += &coeff,
+            Err(not_found) => self.0.insert(not_found, (coeff, var)),
         }
     }
 }
@@ -142,10 +138,21 @@ impl<F: Field> Mul<F> for LinearCombination<F> {
     }
 }
 
+impl<'a, F: Field> Mul<F> for &'a LinearCombination<F> {
+    type Output = LinearCombination<F>;
+
+    #[inline]
+    fn mul(self, scalar: F) -> LinearCombination<F> {
+        let mut cur = self.clone();
+        cur *= scalar;
+        cur
+    }
+}
+
 impl<F: Field> MulAssign<F> for LinearCombination<F> {
     #[inline]
     fn mul_assign(&mut self, scalar: F) {
-        self.0.iter_mut().for_each(|(_, coeff)| *coeff *= &scalar);
+        self.0.iter_mut().for_each(|(coeff, _)| *coeff *= &scalar);
     }
 }
 
@@ -155,6 +162,24 @@ impl<F: Field> Add<Variable> for LinearCombination<F> {
     #[inline]
     fn add(self, other: Variable) -> LinearCombination<F> {
         self + (F::one(), other)
+    }
+}
+
+impl<'a, F: Field> Add<&'a Variable> for LinearCombination<F> {
+    type Output = Self;
+
+    #[inline]
+    fn add(self, other: &'a Variable) -> LinearCombination<F> {
+        self + *other
+    }
+}
+
+impl<'a, F: Field> Sub<&'a Variable> for LinearCombination<F> {
+    type Output = Self;
+
+    #[inline]
+    fn sub(self, other: &'a Variable) -> LinearCombination<F> {
+        self - *other
     }
 }
 
@@ -177,27 +202,27 @@ where
     F1: Fn(F) -> F,
     F2: Fn(F, F) -> F,
 {
-    let mut new_vec = SmallVec::<F>::new(); // with_capacity($self.0.len() + $other.0.len());
+    let mut new_vec = Vec::new();
     let mut i = 0;
     let mut j = 0;
-    while i < cur.0.len() && j < other.0.len() {
-        let self_cur = &cur.0[i];
-        let other_cur = &other.0[j];
-        if self_cur.0 > other_cur.0 {
-            new_vec.push((other.0[j].0, push_fn(other.0[j].1)));
+    while i < cur.len() && j < other.len() {
+        let self_cur = &cur[i];
+        let other_cur = &other[j];
+        if self_cur.1 > other_cur.1 {
+            new_vec.push((push_fn(other[j].0), other[j].1));
             j += 1;
-        } else if self_cur.0 < other_cur.0 {
+        } else if self_cur.1 < other_cur.1 {
             new_vec.push(*self_cur);
             i += 1;
         } else {
-            new_vec.push((self_cur.0, combine_fn(self_cur.1, other_cur.1)));
+            new_vec.push((combine_fn(self_cur.0, other_cur.0), self_cur.1));
             i += 1;
             j += 1;
         }
     }
-    new_vec.extend_from_slice(&cur.0[i..]);
+    new_vec.extend_from_slice(&cur[i..]);
     while j < other.0.len() {
-        new_vec.push((other.0[j].0, push_fn(other.0[j].1)));
+        new_vec.push((push_fn(other[j].0), other[j].1));
         j += 1;
     }
     LinearCombination(new_vec)
