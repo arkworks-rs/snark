3ae0fb38d52da492cd01f1f567594f41d319a7af
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-08-24 00:44:20 -0700

    Refactor `bit` variables in `r1cs-std`

--- r1cs-std/src/bits/mod.rs
+++ r1cs-std/src/bits/mod.rs
@@ -3,75 +3,58 @@ use crate::{
     Vec,
 };
 use algebra::Field;
-use r1cs_core::{ConstraintSystem, SynthesisError};
+use r1cs_core::SynthesisError;
 
 pub mod boolean;
-pub mod uint32;
-pub mod uint64;
 pub mod uint8;
+#[macro_use]
+pub mod uint;
 
-pub trait ToBitsGadget<ConstraintF: Field> {
+make_uint!(UInt16, 16, u16, uint16);
+make_uint!(UInt32, 32, u32, uint32);
+make_uint!(UInt64, 64, u64, uint64);
+
+pub trait ToBitsGadget<F: Field> {
     /// Outputs the canonical bit-wise representation of `self`.
     ///
     /// This is the correct default for 99% of use cases.
-    fn to_bits<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        cs: CS,
-    ) -> Result<Vec<Boolean>, SynthesisError>;
+    fn to_bits(&self) -> Result<Vec<Boolean<F>>, SynthesisError>;
 
     /// Outputs a possibly non-unique bit-wise representation of `self`.
     ///
     /// If you're not absolutely certain that your usecase can get away with a
     /// non-canonical representation, please use `self.to_bits(cs)` instead.
-    fn to_non_unique_bits<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        cs: CS,
-    ) -> Result<Vec<Boolean>, SynthesisError> {
-        self.to_bits(cs)
+    fn to_non_unique_bits(&self) -> Result<Vec<Boolean<F>>, SynthesisError> {
+        self.to_bits()
     }
 }
 
-impl<ConstraintF: Field> ToBitsGadget<ConstraintF> for Boolean {
-    fn to_bits<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        _: CS,
-    ) -> Result<Vec<Boolean>, SynthesisError> {
+impl<F: Field> ToBitsGadget<F> for Boolean<F> {
+    fn to_bits(&self) -> Result<Vec<Boolean<F>>, SynthesisError> {
         Ok(vec![self.clone()])
     }
 }
 
-impl<ConstraintF: Field> ToBitsGadget<ConstraintF> for [Boolean] {
-    fn to_bits<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        _cs: CS,
-    ) -> Result<Vec<Boolean>, SynthesisError> {
+impl<F: Field> ToBitsGadget<F> for [Boolean<F>] {
+    fn to_bits(&self) -> Result<Vec<Boolean<F>>, SynthesisError> {
         Ok(self.to_vec())
     }
 }
 
-impl<ConstraintF: Field> ToBitsGadget<ConstraintF> for Vec<Boolean> {
-    fn to_bits<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        _cs: CS,
-    ) -> Result<Vec<Boolean>, SynthesisError> {
+impl<F: Field> ToBitsGadget<F> for Vec<Boolean<F>> {
+    fn to_bits(&self) -> Result<Vec<Boolean<F>>, SynthesisError> {
         Ok(self.clone())
     }
 }
 
-impl<ConstraintF: Field> ToBitsGadget<ConstraintF> for UInt8 {
-    fn to_bits<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        _cs: CS,
-    ) -> Result<Vec<Boolean>, SynthesisError> {
+impl<F: Field> ToBitsGadget<F> for UInt8<F> {
+    fn to_bits(&self) -> Result<Vec<Boolean<F>>, SynthesisError> {
         Ok(self.into_bits_le())
     }
 }
 
-impl<ConstraintF: Field> ToBitsGadget<ConstraintF> for [UInt8] {
-    fn to_bits<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        _cs: CS,
-    ) -> Result<Vec<Boolean>, SynthesisError> {
+impl<F: Field> ToBitsGadget<F> for [UInt8<F>] {
+    fn to_bits(&self) -> Result<Vec<Boolean<F>>, SynthesisError> {
         let mut result = Vec::with_capacity(&self.len() * 8);
         for byte in self {
             result.extend_from_slice(&byte.into_bits_le());
@@ -80,11 +63,8 @@ impl<ConstraintF: Field> ToBitsGadget<ConstraintF> for [UInt8] {
     }
 }
 
-impl<ConstraintF: Field> ToBitsGadget<ConstraintF> for Vec<UInt8> {
-    fn to_bits<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        _cs: CS,
-    ) -> Result<Vec<Boolean>, SynthesisError> {
+impl<F: Field> ToBitsGadget<F> for Vec<UInt8<F>> {
+    fn to_bits(&self) -> Result<Vec<Boolean<F>>, SynthesisError> {
         let mut result = Vec::with_capacity(&self.len() * 8);
         for byte in self {
             result.extend_from_slice(&byte.into_bits_le());
@@ -93,52 +73,35 @@ impl<ConstraintF: Field> ToBitsGadget<ConstraintF> for Vec<UInt8> {
     }
 }
 
-pub trait ToBytesGadget<ConstraintF: Field> {
+pub trait ToBytesGadget<F: Field> {
     /// Outputs a canonical byte-wise representation of `self`.
     ///
     /// This is the correct default for 99% of use cases.
-    fn to_bytes<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        cs: CS,
-    ) -> Result<Vec<UInt8>, SynthesisError>;
+    fn to_bytes(&self) -> Result<Vec<UInt8<F>>, SynthesisError>;
 
     /// Outputs a possibly non-unique byte decomposition of `self`.
     ///
     /// If you're not absolutely certain that your usecase can get away with a
     /// non-canonical representation, please use `self.to_bytes(cs)` instead.
-    fn to_non_unique_bytes<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        cs: CS,
-    ) -> Result<Vec<UInt8>, SynthesisError> {
-        self.to_bytes(cs)
+    fn to_non_unique_bytes(&self) -> Result<Vec<UInt8<F>>, SynthesisError> {
+        self.to_bytes()
     }
 }
 
-impl<ConstraintF: Field> ToBytesGadget<ConstraintF> for [UInt8] {
-    fn to_bytes<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        _cs: CS,
-    ) -> Result<Vec<UInt8>, SynthesisError> {
+impl<F: Field> ToBytesGadget<F> for [UInt8<F>] {
+    fn to_bytes(&self) -> Result<Vec<UInt8<F>>, SynthesisError> {
         Ok(self.to_vec())
     }
 }
 
-impl<'a, ConstraintF: Field, T: 'a + ToBytesGadget<ConstraintF>> ToBytesGadget<ConstraintF>
-    for &'a T
-{
-    fn to_bytes<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        cs: CS,
-    ) -> Result<Vec<UInt8>, SynthesisError> {
-        (*self).to_bytes(cs)
+impl<'a, F: Field, T: 'a + ToBytesGadget<F>> ToBytesGadget<F> for &'a T {
+    fn to_bytes(&self) -> Result<Vec<UInt8<F>>, SynthesisError> {
+        (*self).to_bytes()
     }
 }
 
-impl<'a, ConstraintF: Field> ToBytesGadget<ConstraintF> for &'a [UInt8] {
-    fn to_bytes<CS: ConstraintSystem<ConstraintF>>(
-        &self,
-        _cs: CS,
-    ) -> Result<Vec<UInt8>, SynthesisError> {
+impl<'a, F: Field> ToBytesGadget<F> for &'a [UInt8<F>] {
+    fn to_bytes(&self) -> Result<Vec<UInt8<F>>, SynthesisError> {
         Ok(self.to_vec())
     }
 }
