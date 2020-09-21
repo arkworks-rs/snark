19a01e6fd580cbf912387fad70c3928c39022c11
weikeng <w.k@berkeley.edu>
2020-09-20 18:17:23 -0700

    [attempt] to_bits constant len

--- r1cs-std/src/fields/fp/mod.rs
+++ r1cs-std/src/fields/fp/mod.rs
@@ -873,7 +873,7 @@ impl<F: PrimeField> ToBitsGadget<F> for FpVar<F> {
         use algebra::BitIteratorLE;
         match self {
             Self::Constant(c) => Ok(BitIteratorLE::new(&c.into_repr())
-                .keep(F::Params::MODULUS_BITS - 1)
+                .take((F::Params::MODULUS_BITS - 1) as usize)
                 .map(Boolean::constant)
                 .collect::<Vec<_>>()),
             Self::Var(v) => v.to_non_unique_bits_le(),
