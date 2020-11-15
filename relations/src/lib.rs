cac553db8390e790219dc2aeeb07bae5ca0116c4
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-11-15 12:48:39 -0800

    Add `arkworks-rs/snark` code

---
+++ relations/src/lib.rs
@@ -0,0 +1,19 @@
+//! Core interface for working with various relations that are useful in
+//! zkSNARKs. At the moment, we only implement APIs for working with Rank-1
+//! Constraint Systems (R1CS).
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![deny(
+    warnings,
+    unused,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    missing_docs
+)]
+#![deny(unsafe_code)]
+
+#[macro_use]
+extern crate ark_std;
+
+pub mod r1cs;
---
+++ relations/src/r1cs/mod.rs
@@ -0,0 +1,148 @@
+//! Core interface for working with Rank-1 Constraint Systems (R1CS).
+
+use ark_std::vec::Vec;
+
+/// A result type specialized to `SynthesisError`.
+pub type Result<T> = core::result::Result<T, SynthesisError>;
+
+#[macro_use]
+mod impl_lc;
+mod constraint_system;
+mod error;
+#[cfg(feature = "std")]
+mod trace;
+
+#[cfg(feature = "std")]
+pub use crate::r1cs::trace::{ConstraintLayer, ConstraintTrace, TraceStep, TracingMode};
+
+pub use tracing::info_span;
+
+pub use ark_ff::{Field, ToConstraintField};
+pub use constraint_system::{
+    ConstraintMatrices, ConstraintSynthesizer, ConstraintSystem, ConstraintSystemRef, Namespace,
+    SynthesisMode,
+};
+pub use error::SynthesisError;
+
+use core::cmp::Ordering;
+
+/// A sparse representation of constraint matrices.
+pub type Matrix<F> = Vec<Vec<(F, usize)>>;
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+/// An opaque counter for symbolic linear combinations.
+pub struct LcIndex(usize);
+
+/// Represents the different kinds of variables present in a constraint system.
+#[derive(Copy, Clone, PartialEq, Debug, Eq)]
+pub enum Variable {
+    /// Represents the "zero" constant.
+    Zero,
+    /// Represents of the "one" constant.
+    One,
+    /// Represents a public instance variable.
+    Instance(usize),
+    /// Represents a private witness variable.
+    Witness(usize),
+    /// Represents of a linear combination.
+    SymbolicLc(LcIndex),
+}
+
+/// A linear combination of variables according to associated coefficients.
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+pub struct LinearCombination<F: Field>(pub Vec<(F, Variable)>);
+
+/// Generate a `Namespace` with name `name` from `ConstraintSystem` `cs`.
+/// `name` must be a `&'static str`.
+#[macro_export]
+macro_rules! ns {
+    ($cs:expr, $name:expr) => {{
+        let span = $crate::r1cs::info_span!(target: "r1cs", $name);
+        let id = span.id();
+        let _enter_guard = span.enter();
+        core::mem::forget(_enter_guard);
+        core::mem::forget(span);
+        $crate::r1cs::Namespace::new($cs.clone(), id)
+    }};
+}
+
+impl Variable {
+    /// Is `self` the zero variable?
+    #[inline]
+    pub fn is_zero(&self) -> bool {
+        matches!(self, Variable::Zero)
+    }
+
+    /// Is `self` the one variable?
+    #[inline]
+    pub fn is_one(&self) -> bool {
+        matches!(self, Variable::One)
+    }
+
+    /// Is `self` an instance variable?
+    #[inline]
+    pub fn is_instance(&self) -> bool {
+        matches!(self, Variable::Instance(_))
+    }
+
+    /// Is `self` a witness variable?
+    #[inline]
+    pub fn is_witness(&self) -> bool {
+        matches!(self, Variable::Witness(_))
+    }
+
+    /// Is `self` a linear combination?
+    #[inline]
+    pub fn is_lc(&self) -> bool {
+        matches!(self, Variable::SymbolicLc(_))
+    }
+
+    /// Get the `LcIndex` in `self` if `self.is_lc()`.
+    #[inline]
+    pub fn get_lc_index(&self) -> Option<LcIndex> {
+        match self {
+            Variable::SymbolicLc(index) => Some(*index),
+            _ => None,
+        }
+    }
+
+    /// Returns `Some(usize)` if `!self.is_lc()`, and `None` otherwise.
+    #[inline]
+    pub fn get_index_unchecked(&self, witness_offset: usize) -> Option<usize> {
+        match self {
+            // The one variable always has index 0
+            Variable::One => Some(0),
+            Variable::Instance(i) => Some(*i),
+            Variable::Witness(i) => Some(witness_offset + *i),
+            _ => None,
+        }
+    }
+}
+
+impl PartialOrd for Variable {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        use Variable::*;
+        match (self, other) {
+            (Zero, Zero) => Some(Ordering::Equal),
+            (One, One) => Some(Ordering::Equal),
+            (Zero, _) => Some(Ordering::Less),
+            (One, _) => Some(Ordering::Less),
+            (_, Zero) => Some(Ordering::Greater),
+            (_, One) => Some(Ordering::Greater),
+
+            (Instance(i), Instance(j)) | (Witness(i), Witness(j)) => i.partial_cmp(j),
+            (Instance(_), Witness(_)) => Some(Ordering::Less),
+            (Witness(_), Instance(_)) => Some(Ordering::Greater),
+
+            (SymbolicLc(i), SymbolicLc(j)) => i.partial_cmp(j),
+            (_, SymbolicLc(_)) => Some(Ordering::Less),
+            (SymbolicLc(_), _) => Some(Ordering::Greater),
+        }
+    }
+}
+
+impl Ord for Variable {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
