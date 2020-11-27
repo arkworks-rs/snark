91aa9122baf1ee2def5849919feb67090d9d848c
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-11-27 10:34:30 -0800

    Reduce memory consumption during inlining

--- relations/src/r1cs/constraint_system.rs
+++ relations/src/r1cs/constraint_system.rs
@@ -238,6 +238,24 @@ impl<F: Field> ConstraintSystem<F> {
         Ok(())
     }
 
+    /// Count the number of times a given LC is used within another LC
+    fn lc_num_times_used(&self, count_sinks: bool) -> Vec<usize> {
+        // step 1: Identify all lcs that have been many times
+        let mut num_times_used = vec![0; self.lc_map.len()];
+
+        for (index, lc) in self.lc_map.iter() {
+            num_times_used[index.0] += count_sinks as usize;
+
+            for &(_, var) in lc.iter() {
+                if var.is_lc() {
+                    let lc_index = var.get_lc_index().expect("should be lc");
+                    num_times_used[lc_index.0] += 1;
+                }
+            }
+        }
+        num_times_used
+    }
+
     /// Naively inlines symbolic linear combinations into the linear
     /// combinations that use them.
     ///
@@ -248,6 +266,8 @@ impl<F: Field> ConstraintSystem<F> {
     /// is the dominating cost.
     pub fn inline_all_lcs(&mut self) {
         let mut inlined_lcs = BTreeMap::new();
+        let mut num_times_used = self.lc_num_times_used(false);
+
         for (&index, lc) in &self.lc_map {
             let mut inlined_lc = LinearCombination::new();
             for &(coeff, var) in lc.iter() {
@@ -257,6 +277,12 @@ impl<F: Field> ConstraintSystem<F> {
                     // inlined LC, and substitute it in.
                     let lc = inlined_lcs.get(&lc_index).expect("should be inlined");
                     inlined_lc.extend((lc * coeff).0.into_iter());
+
+                    num_times_used[lc_index.0] -= 1;
+                    if num_times_used[lc_index.0] == 0 {
+                        // This lc is not used any more, so remove it.
+                        inlined_lcs.remove(&lc_index);
+                    }
                 } else {
                     // Otherwise, it's a concrete variable and so we
                     // substitute it in directly.
@@ -282,19 +308,7 @@ impl<F: Field> ConstraintSystem<F> {
             return;
         }
 
-        // step 1: Identify all lcs that have been many times
-        let mut used_times = vec![0; self.lc_map.len()];
-
-        for (index, lc) in self.lc_map.iter() {
-            used_times[index.0] += 1;
-
-            for &(_, var) in lc.iter() {
-                if var.is_lc() {
-                    let lc_index = var.get_lc_index().expect("should be lc");
-                    used_times[lc_index.0] += 1;
-                }
-            }
-        }
+        let used_times = self.lc_num_times_used(true);
 
         // step 2: Modify the lcs accordingly
         let mut outlined_lcs = BTreeMap::new();
