0a1937c1aefe90e223e89458dd78a11f1d78a53d
Phoinic <max@phoinic.com.ua>
2020-12-28 23:14:43 +0200

    log2 function added

--- algebra/src/lib.rs
+++ algebra/src/lib.rs
@@ -41,3 +41,13 @@ mod to_field_vec;
 pub use to_field_vec::ToConstraintField;
 
 pub type Error = Box<dyn std::error::Error>;
+
+/// Returns log2
+pub fn log2(x: usize) -> u32 {
+    if x <= 1 {
+        return 0;
+    }
+
+    let n = x.leading_zeros();
+    core::mem::size_of::<usize>() as u32 * 8 - n
+}
