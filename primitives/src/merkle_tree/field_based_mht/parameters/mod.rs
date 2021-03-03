7328cc79ad11db875d689303e1a6dee2c03d5b9c
DanieleDiBenedetto <daniele@horizenlabs.io>
2021-03-03 09:30:51 -0500

    Added FieldBasedMHTParameters for BN382

--- primitives/src/merkle_tree/field_based_mht/parameters/mod.rs
+++ primitives/src/merkle_tree/field_based_mht/parameters/mod.rs
@@ -7,3 +7,8 @@ pub use self::mnt4753::*;
 pub mod mnt6753;
 #[cfg(feature = "mnt6_753")]
 pub use self::mnt6753::*;
+
+#[cfg(feature = "bn_382")]
+pub mod bn382;
+#[cfg(feature = "bn_382")]
+pub use self::bn382::*;
