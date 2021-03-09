691ad918d34b5fa35a33f104d24a2ed744a78774
Paolo Tagliaferri <paolotagliaferri@horizenlabs.io>
2021-03-09 10:58:05 +0100

    Added MHT parameters for TweedleFr.

--- primitives/src/merkle_tree/field_based_mht/parameters/mod.rs
+++ primitives/src/merkle_tree/field_based_mht/parameters/mod.rs
@@ -7,3 +7,8 @@ pub use self::mnt4753::*;
 pub mod mnt6753;
 #[cfg(feature = "mnt6_753")]
 pub use self::mnt6753::*;
+
+#[cfg(feature = "tweedle")]
+pub mod tweedle_fr;
+#[cfg(feature = "tweedle")]
+pub use self::tweedle_fr::*;
