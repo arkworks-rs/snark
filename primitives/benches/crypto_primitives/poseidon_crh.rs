b462608501da163696386ffa60463848e4c1f175
DanieleDiBenedetto <daniele@horizenlabs.io>
2021-06-18 16:28:26 +0200

    Other fixes

--- primitives/benches/crypto_primitives/poseidon_crh.rs
+++ primitives/benches/crypto_primitives/poseidon_crh.rs
@@ -21,7 +21,7 @@ use primitives::crh::{
 fn poseidon_crh_eval_mnt4(c: &mut Criterion) {
 
     let mut rng = XorShiftRng::seed_from_u64(1231275789u64);
-    let samples = 2000;
+    let samples = 10;
     let mut h = MNT4PoseidonHash::init_constant_length(samples, None);
 
     c.bench_function("Poseidon CRH Eval for MNT4", move |b| {
@@ -30,6 +30,7 @@ fn poseidon_crh_eval_mnt4(c: &mut Criterion) {
                 h.update(MNT4753Fr::rand(&mut rng));
             }
             h.finalize().unwrap();
+            h.reset(None);
         })
     });
 }
@@ -37,7 +38,7 @@ fn poseidon_crh_eval_mnt4(c: &mut Criterion) {
 fn poseidon_crh_eval_mnt6(c: &mut Criterion) {
 
     let mut rng = XorShiftRng::seed_from_u64(1231275789u64);
-    let samples = 2000;
+    let samples = 10;
     let mut h = MNT6PoseidonHash::init_constant_length(samples, None);
 
     c.bench_function("Poseidon CRH Eval for MNT6", move |b| {
@@ -46,6 +47,7 @@ fn poseidon_crh_eval_mnt6(c: &mut Criterion) {
                 h.update(MNT6753Fr::rand(&mut rng));
             }
             h.finalize().unwrap();
+            h.reset(None);
         })
     });
 }
@@ -53,7 +55,7 @@ fn poseidon_crh_eval_mnt6(c: &mut Criterion) {
 fn poseidon_crh_eval_bn382fr(c: &mut Criterion) {
 
     let mut rng = XorShiftRng::seed_from_u64(1231275789u64);
-    let samples = 2000;
+    let samples = 10;
     let mut h = BN382FrPoseidonHash::init_constant_length(samples, None);
 
     c.bench_function("Poseidon CRH Eval for BN382Fr", move |b| {
@@ -62,6 +64,7 @@ fn poseidon_crh_eval_bn382fr(c: &mut Criterion) {
                 h.update(BN382Fr::rand(&mut rng));
             }
             h.finalize().unwrap();
+            h.reset(None);
         })
     });
 }
@@ -69,7 +72,7 @@ fn poseidon_crh_eval_bn382fr(c: &mut Criterion) {
 fn poseidon_crh_eval_bn382fq(c: &mut Criterion) {
 
     let mut rng = XorShiftRng::seed_from_u64(1231275789u64);
-    let samples = 2000;
+    let samples = 10;
     let mut h = BN382FqPoseidonHash::init_constant_length(samples, None);
 
     c.bench_function("Poseidon CRH Eval for BN382Fq", move |b| {
@@ -78,6 +81,7 @@ fn poseidon_crh_eval_bn382fq(c: &mut Criterion) {
                 h.update(BN382Fq::rand(&mut rng));
             }
             h.finalize().unwrap();
+            h.reset(None);
         })
     });
 }
@@ -85,7 +89,7 @@ fn poseidon_crh_eval_bn382fq(c: &mut Criterion) {
 fn poseidon_crh_eval_tweedlefr(c: &mut Criterion) {
 
     let mut rng = XorShiftRng::seed_from_u64(1231275789u64);
-    let samples = 2000;
+    let samples = 10;
     let mut h = TweedleFrPoseidonHash::init_constant_length(samples, None);
 
     c.bench_function("Poseidon CRH Eval for tweedleFr", move |b| {
@@ -94,6 +98,7 @@ fn poseidon_crh_eval_tweedlefr(c: &mut Criterion) {
                 h.update(tweedleFr::rand(&mut rng));
             }
             h.finalize().unwrap();
+            h.reset(None);
         })
     });
 }
@@ -101,7 +106,7 @@ fn poseidon_crh_eval_tweedlefr(c: &mut Criterion) {
 fn poseidon_crh_eval_tweedlefq(c: &mut Criterion) {
 
     let mut rng = XorShiftRng::seed_from_u64(1231275789u64);
-    let samples = 2000;
+    let samples = 10;
     let mut h = TweedleFqPoseidonHash::init_constant_length(samples, None);
 
     c.bench_function("Poseidon CRH Eval for tweedleFq", move |b| {
@@ -110,6 +115,7 @@ fn poseidon_crh_eval_tweedlefq(c: &mut Criterion) {
                 h.update(tweedleFq::rand(&mut rng));
             }
             h.finalize().unwrap();
+            h.reset(None);
         })
     });
 }
@@ -117,7 +123,7 @@ fn poseidon_crh_eval_tweedlefq(c: &mut Criterion) {
 
 criterion_group! {
     name = crh_poseidon_eval;
-    config = Criterion::default().sample_size(20);
+    config = Criterion::default().sample_size(10);
     targets = poseidon_crh_eval_mnt4, poseidon_crh_eval_mnt6,
               poseidon_crh_eval_bn382fq, poseidon_crh_eval_bn382fr,
               poseidon_crh_eval_tweedlefq, poseidon_crh_eval_tweedlefr,
