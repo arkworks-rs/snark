ea61a32d9997fcc67757a48f0b1080d818f3cc57
cronicc <cronic@zensystem.io>
2021-10-06 14:04:53 +0000

    Login to hub.docker.com read-only account to fix pull rate limiting

--- ci/script.sh
+++ ci/script.sh
@@ -15,6 +15,9 @@ export CARGO_AUDIT_EXIT_ON_ERROR="${CARGO_AUDIT_EXIT_ON_ERROR:-true}"
 # run tests in docker or natively
 if [ -n "${TESTS:-}" ]; then
   if [ "$have_docker" = "true" ]; then
+    if [ -n "${DOCKER_READONLY_USERNAME:-}" ] && [ -n "${DOCKER_READONLY_PASSWORD:-}" ]; then
+      echo "$DOCKER_READONLY_PASSWORD" | docker login -u "$DOCKER_READONLY_USERNAME" --password-stdin
+    fi
     docker run --rm -t -v "$workdir":/build --entrypoint /build/ci/docker/entrypoint.sh -e TESTS -e CARGO_AUDIT_EXIT_ON_ERROR -e LOCAL_USER_ID="$(id -u)" \
       -e LOCAL_GRP_ID="$(id -g)" --env-file <(env | grep -E '^(RUSTFLAGS=|CARGOARGS=|RUST_CROSS_TARGETS=|RUSTUP_TOOLCHAIN=).+$') \
       "$image" /build/ci/run_tests.sh
