0a91b7d2222edabcd402d4dc1462e5b65316269e
Pratyush Mishra <pratyushmishra@berkeley.edu>
2025-03-18 16:40:56 -0400

    Improve

--- relations/src/gr1cs/predicate/polynomial_constraint.rs
+++ relations/src/gr1cs/predicate/polynomial_constraint.rs
@@ -1,5 +1,5 @@
 //! This module contains the implementation of the Polynomial Predicate struct.
-//! A polynomial predicate is a kind of  predicate which is defined in https://eprint.iacr.org/2024/1245
+//! A polynomial predicate is a kind of  predicate which is defined in https://eprint.iacr.org/2024/1245.
 //! Other kinds of  predicates can be added in the future such as lookup
 //! table predicates.
 
@@ -42,31 +42,33 @@ impl<F: Field> PolynomialPredicate<F> {
 /// The evaluation of a polynomial predicate is the evaluation of the underlying
 /// polynomial and the arity is the number of variables in the polynomial.
 impl<F: Field> PolynomialPredicate<F> {
-    /// Check if the predicate is satisfied by the given variables
+    /// Check if the predicate is satisfied by the given variables.
     pub fn is_satisfied(&self, variables: &[F]) -> bool {
         // TODO: Change the polynomial eval to get a slice as an evaluation point
         !self.polynomial.evaluate(&variables.to_vec()).is_zero()
     }
-    /// What is the evaluation of the polynomial predicate given the variables
+
+    /// Evaluate the predicate on the given variables.
     pub fn eval(&self, variables: &[F]) -> F {
         self.polynomial.evaluate(&variables.to_vec())
     }
 
-    /// Get the arity of the polynomial predicate
-    /// The arity of P(x1, x2, ..., xn) is n
+    /// Get the arity of the polynomial predicate.
+    /// For example, the arity of P(x1, x2, ..., xn) is n.
     pub fn arity(&self) -> usize {
         self.polynomial.num_vars()
     }
 
-    /// Get the degree of the polynomial predicate
-    /// The degree of x1 + x2^4 + x3^2 is 4
+    /// Get the degree of the polynomial predicate.
+    /// For example, the degree of x1 + x2^4 + x3^2 is 4.
     pub fn degree(&self) -> usize {
         self.polynomial.degree()
     }
 }
 
-/// The label for the popular R1CS predicate
+/// A label for the R1CS predicate.
 pub const R1CS_PREDICATE_LABEL: &str = "R1CS";
 
-/// The label for the Squared R1CS predicate introduced in https://eprint.iacr.org/2017/540
+/// A label for the Square R1CS predicate introduced in 
+/// [\[Groth-Maller17\]](https://eprint.iacr.org/2017/540).
 pub const SR1CS_PREDICATE_LABEL: &str = "SR1CS";
